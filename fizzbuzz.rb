b5f94bc0d2af2132dd3cb92593461337fd542ef9
SriviB <srivibalaji33@gmail.com>
2018-07-25 14:11:59 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 != 0
+    print "Fizz"
+  elsif num % 5 == 0 && num % 3 != 0
+    print "Buzz"
+  elsif num % 3 == 0 && num % 5 == 0
+    print "FizzBuzz"
+  else
+    print "nil"
+  end
+end
+
+fizzbuzz(15)
